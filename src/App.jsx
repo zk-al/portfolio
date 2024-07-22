7f48707a406dfedf01261898c5fddfa89cbd5198
Zack <lamarche.zack@gmail.com>
2024-07-21 18:46:24 -0700

    Fix errors :adhesive_bandage: Add header and hero base HTML :lipstick:

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.scss";
+import logo from "./assets/svg/logo.svg";
 
 function App() {
   return (
     <div>
-      
+      <header className="header">
+        <img className="header__logo" src={logo} alt="" />
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a className="nav__link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#">
+                Work
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="hero">
+        <div>
+          <svg>Blob 1</svg>
+          <svg>Line</svg>
+          <svg>Blob 2</svg>
+        </div>
+
+        <div>
+          <h1>Designer to Developer</h1>
+          <h2>Out here tryin' to have a good time</h2>
+        </div>
+      </section>
     </div>
   );
 }
